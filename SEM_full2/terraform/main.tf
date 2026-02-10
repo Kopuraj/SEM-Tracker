cd635dcbb4358d34d7ba281df7eaab419a720610
Kopuraj <thiruchelvamkopuraj@gmail.com>
2026-02-10 13:44:27 +0530

    update

--- SEM_full2/terraform/main.tf
+++ SEM_full2/terraform/main.tf
@@ -76,8 +76,8 @@ resource "aws_security_group" "sem_tracker_sg" {
   
   # Backend API (if needed)
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "Backend API"
